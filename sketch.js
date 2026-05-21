1994711c3c0a8401eaac3c8c661fb55ba587fb80
anni-cool <geierstangera0837@my.sduhsd.net>
2026-05-21 19:27:40 +0000

    made changes to the coin function

--- sketch.js
+++ sketch.js
@@ -1,6 +1,26 @@
 await Canvas(0, 0, 400, 500);
 world.gravity.y = 9.8;
 
+const coinImagePath = 'assets/coin.png';
+let coinImage = null;
+
+if (typeof loadImage === 'function') {
+	try {
+		let loaded = loadImage(coinImagePath);
+		if (loaded && typeof loaded.then === 'function') {
+			loaded.then((img) => {
+				coinImage = img;
+			}).catch(() => {
+				coinImage = null;
+			});
+		} else {
+			coinImage = loaded || null;
+		}
+	} catch (error) {
+		coinImage = null;
+	}
+}
+
 let ground = new Group();
 ground.physics = STATIC;
 ground.d = 24;
@@ -8,14 +28,8 @@ ground.img = '🟫';
 ground.tile = '=';
 ground.bounciness = 0;
 
-let coins = new Group();
-coins.physics = STATIC;
-coins.d = 24;
-coins.img = '🟡';
-coins.tile = 'o';
-
 let tiles = [
-	'...............o.............o........oo...........o...o..................',
+	'..........................................................................',
 	'=========================================================================='
 ];
 
@@ -29,29 +43,46 @@ ladybug.rotationLock = true;
 ladybug.friction = 0.1;
 ladybug.bounciness = 0;
 
+const coinPickupRadius = 20;
+const coinStepX = 180;
+
 
 q5.update = function move() {
 	background('skyblue');
 
-	if (ladybug.colliding(level1)) {
-		if (kb.presses('up')) {
-			ladybug.vel= [1,-7];
+	if (coin.visible) {
+		if (coinImage && typeof image === 'function') {
+			imageMode(CENTER);
+			image(coinImage, coin.x, coin.y, coin.size, coin.size);
+		} else {
+			stroke('#8a6b00');
+			strokeWeight(2);
+			fill('#ffd54a');
+			circle(coin.x, coin.y, coin.size);
 		}
 	}
-	if (kb.presses('right')) {
+
+	if (ladybug.colliding(level1)) {
+		if (kb.presses('up')) {
+			ladybug.vel.y = -7;
+		}
+	}
+	if (kb.pressing('right')) {
 		ladybug.direction = 0;
 		ladybug.vel.x = 5;
 	}
-	else if (kb.presses('left')) {
+	else if (kb.pressing('left')) {
 		ladybug.direction = 180;
 		ladybug.vel.x = -5;
 	}
 
-	let touchedCoins = ladybug.overlaps(coins);
-
-
-	if (touchedCoins) {
-		touchedCoins.remove();
+	if (coin.visible) {
+		let distanceToCoin = dist(ladybug.x, ladybug.y, coin.x, coin.y);
+		if (distanceToCoin <= coinPickupRadius) {
+			coin.x += coinStepX;
+			coin.y = random(120, 220);
+			
+		}
 	}
 };
 
