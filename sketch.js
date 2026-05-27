c7b29fb89edacda4066dbd2b00b6422d544bac16
anni-cool <geierstangera0837@my.sduhsd.net>
2026-05-27 20:47:28 +0000

    I made it scaleable and attuned to all window sizes

--- sketch.js
+++ sketch.js
@@ -2,15 +2,20 @@ await Canvas();
 world.gravity.y = 17;
 textFont('Comic Sans MS');
 
+const BASE_HALF_W = 700;
+const BASE_HALF_H = 600;
+const UI_Y_SHIFT = -120;
+const INSPIRATION_PAGE_SCALE = 1.06;
+
 let discoBallImg = await loadImage('assets/discoBall.png');
 
 // Calculated the positions of the Disco and the text 
-let winDiscoX = width / 2-700;
-let winDiscoY = height / 2 - 600;
+let winDiscoX = BASE_HALF_W - 700;
+let winDiscoY = BASE_HALF_H - 600 + UI_Y_SHIFT;
 let winDiscoWidth = 260;
 let winDiscoHeight = 260;
-let winResetTextX = width / 2-700;
-let winResetTextY = height / 2 -750;
+let winResetTextX = BASE_HALF_W - 700;
+let winResetTextY = BASE_HALF_H - 750 + UI_Y_SHIFT;
 
 //physics for the ground (img 1)
 let ground1 = new Group();
@@ -74,9 +79,9 @@ let showTextPage = false;
 const infoPageYOffset = -100;
 
 //buttons for the intro and instructions pages
-let instructionsButton = { x: width / 2 - 900, y: height / 2 - 500, w: 440, h: 80 };
-let textPageButton = { x: width / 2 - 900, y: height / 2 - 400, w: 440, h: 80 };
-let introBackButton = { x: width / 2 - 900, y: height / 2 - 20 + infoPageYOffset, w: 440, h: 80 };
+let instructionsButton = { x: BASE_HALF_W - 900, y: BASE_HALF_H - 500 + UI_Y_SHIFT, w: 440, h: 80 };
+let textPageButton = { x: BASE_HALF_W - 900, y: BASE_HALF_H - 400 + UI_Y_SHIFT, w: 440, h: 80 };
+let introBackButton = { x: BASE_HALF_W - 900, y: BASE_HALF_H - 20 + infoPageYOffset + UI_Y_SHIFT, w: 440, h: 80 };
 background('skyblue');
 
 //ladybug sprite 
@@ -161,12 +166,19 @@ function drawGameplaySky() {
     camera.on();
 }
 
-function isMouseInsideButton(button) {
+function getUIScale() {
+    return min(width / (BASE_HALF_W * 2), height / (BASE_HALF_H * 2), 1);
+}
+
+function isMouseInsideButton(button, scaleFactor = getUIScale()) {
+    const scaledMouseX = mouse.x / scaleFactor;
+    const scaledMouseY = mouse.y / scaleFactor;
+
     return (
-        mouse.x >= button.x &&
-        mouse.x <= button.x + button.w &&
-        mouse.y >= button.y &&
-        mouse.y <= button.y + button.h
+        scaledMouseX >= button.x &&
+        scaledMouseX <= button.x + button.w &&
+        scaledMouseY >= button.y &&
+        scaledMouseY <= button.y + button.h
     );
 }
 
@@ -204,7 +216,7 @@ q5.update = function move() {
     if (showTextPage) {
         if (kb.presses('q')) openIntro();
         if (kb.presses('enter')) openGame();
-        if (typeof mouse !== 'undefined' && mouse.presses && mouse.presses() && isMouseInsideButton(introBackButton)) {
+        if (typeof mouse !== 'undefined' && mouse.presses && mouse.presses() && isMouseInsideButton(introBackButton, getUIScale() * INSPIRATION_PAGE_SCALE)) {
             openIntro();
         }
         return;
@@ -281,16 +293,19 @@ q5.update = function move() {
 
 q5.draw = function() {
     camera.off();
+    const uiScale = getUIScale();
 
     if (showIntro) {
         background('#dc0d4b');
+        push();
+        scale(uiScale);
         fill('white');
         stroke('black');
         strokeWeight(4);
         textAlign(CENTER, CENTER);
 
         textSize(98);
-        text('Ladybug Disco', 0, -250);
+        text('Ladybug Disco', 0, -250 + UI_Y_SHIFT);
 
         fill('#ffffff');
         rect(instructionsButton.x, instructionsButton.y, instructionsButton.w, instructionsButton.h, 16);
@@ -305,32 +320,35 @@ q5.draw = function() {
 
         fill('white');
         textSize(36);
-        text('Press ENTER to start game', width / 2 - 700, height / 2 - 600);
+        text('Press ENTER to start game', 0, -70 + UI_Y_SHIFT);
 
+        pop();
         camera.on();
         return;
     }
 
     if (showInstructions) {
         background('#3a8edb');
+        push();
+        scale(uiScale);
         fill('white');
         stroke('black');
         strokeWeight(3);
         textAlign(CENTER, CENTER);
 
         textSize(72);
-            text('HOW TO PLAY', width / 2 - 700, height / 2 - 600 + infoPageYOffset);
+            text('HOW TO PLAY', BASE_HALF_W - 700, BASE_HALF_H - 630 + infoPageYOffset + UI_Y_SHIFT);
 
                 textSize(34);
-            text('Move: LEFT and RIGHT arrows', width / 2 - 700, height / 2 - 500 + infoPageYOffset);
-            text('Jump: UP arrow', width / 2 - 700, height / 2 - 450 + infoPageYOffset);
-            text('Collect all 20 coins to win and go to the disco', width / 2 - 700, height / 2 - 400 + infoPageYOffset);
-            text('Press Q to view instructions at any time', width / 2 - 700, height / 2 - 350 + infoPageYOffset);
-            text('Press Q to go back to gameplay', width / 2 - 700, height / 2 - 300 + infoPageYOffset);
-            text('Refresh the page to restart', width / 2 - 700, height / 2 - 250 + infoPageYOffset);
+            text('Move: LEFT and RIGHT arrows', BASE_HALF_W - 700, BASE_HALF_H - 500 + infoPageYOffset + UI_Y_SHIFT);
+            text('Jump: UP arrow', BASE_HALF_W - 700, BASE_HALF_H - 450 + infoPageYOffset + UI_Y_SHIFT);
+            text('Collect all 20 coins to win and go to the disco', BASE_HALF_W - 700, BASE_HALF_H - 400 + infoPageYOffset + UI_Y_SHIFT);
+            text('Press Q to view instructions at any time', BASE_HALF_W - 700, BASE_HALF_H - 350 + infoPageYOffset + UI_Y_SHIFT);
+            text('Press Q to go back to gameplay', BASE_HALF_W - 700, BASE_HALF_H - 300 + infoPageYOffset + UI_Y_SHIFT);
+            text('Refresh the page to restart', BASE_HALF_W - 700, BASE_HALF_H - 250 + infoPageYOffset + UI_Y_SHIFT);
 
                 textSize(30);
-            text('Click below to return to the intro page', width / 2 - 700, height / 2 - 150 + infoPageYOffset);
+            text('Click below to return to the intro page', BASE_HALF_W - 700, BASE_HALF_H - 90 + infoPageYOffset + UI_Y_SHIFT);
 
         fill('#ffffff');
         rect(introBackButton.x, introBackButton.y, introBackButton.w, introBackButton.h, 16);
@@ -339,32 +357,34 @@ q5.draw = function() {
         textSize(34);
         text('Back to Intro Page', introBackButton.x + introBackButton.w / 2, introBackButton.y + introBackButton.h / 2);
 
+        pop();
         camera.on();
         return;
     }
 
     if (showTextPage) {
         background('#0f5b9e');
+        push();
+        scale(uiScale * INSPIRATION_PAGE_SCALE);
         fill('white');
         stroke('black');
         strokeWeight(3);
         textAlign(CENTER, CENTER);
 
 
-         textSize(72);
-            text('Our Inspiration for Ladybug Disco', width / 2 - 700, height / 2 - 650 + infoPageYOffset);
+         textSize(64);
+            text('Our Inspiration for Ladybug Disco', BASE_HALF_W - 700, BASE_HALF_H - 650 + infoPageYOffset + UI_Y_SHIFT);
 
-                textSize(24);
-            text('Our game, Ladybug Disco, was inspired by both Adventure Island and Pacman. The ladybug, like Master Higgins', width / 2 - 700, height / 2 - 550 + infoPageYOffset);
-            text('from Adventure Island, must traverse through obstacles by using keyboard keys to jump and move forward.', width / 2 - 700, height / 2 - 500 + infoPageYOffset);
-            text(' Additionally, we were motivated by the coin collection mechanism of Pacman. The ladybug must first collect', width / 2 - 700, height / 2 - 450 + infoPageYOffset);
-            text('all the coins before moving on towards the disco, similarly in Pacman, it must collect all the coins before', width / 2 - 700, height / 2 - 400 + infoPageYOffset);
-            text('moving towards the next level. Both games inspired the mechanisms ingrained in the code of Ladybug Disco, ', width / 2 - 700, height / 2 - 350 + infoPageYOffset);
-            text('however, we added unique sprites and a special theme to cultivate the final product.', width / 2 - 700, height / 2 - 300 + infoPageYOffset);
-            text('Press ENTER to reset after winning', width / 2 - 700, height / 2 -150 + infoPageYOffset);
+                textSize(21);
+            text('Our game, Ladybug Disco, was inspired by both Adventure Island and Pacman. The ladybug, like Master Higgins', BASE_HALF_W - 700, BASE_HALF_H - 550 + infoPageYOffset + UI_Y_SHIFT);
+            text('from Adventure Island, must traverse through obstacles by using keyboard keys to jump and move forward.', BASE_HALF_W - 700, BASE_HALF_H - 510 + infoPageYOffset + UI_Y_SHIFT);
+            text(' Additionally, we were motivated by the coin collection mechanism of Pacman. The ladybug must first collect', BASE_HALF_W - 700, BASE_HALF_H - 470 + infoPageYOffset + UI_Y_SHIFT);
+            text('all the coins before moving on towards the disco, similarly in Pacman, it must collect all the coins before', BASE_HALF_W - 700, BASE_HALF_H - 430 + infoPageYOffset + UI_Y_SHIFT);
+            text('moving towards the next level. Both games inspired the mechanisms ingrained in the code of Ladybug Disco, ', BASE_HALF_W - 700, BASE_HALF_H - 390 + infoPageYOffset + UI_Y_SHIFT);
+            text('however, we added unique sprites and a special theme to cultivate the final product.', BASE_HALF_W - 700, BASE_HALF_H - 350 + infoPageYOffset + UI_Y_SHIFT);
 
                 textSize(30);
-            text('Click below to return to the intro page', width / 2 - 700, height / 2 - 100 + infoPageYOffset);
+            text('Click below to return to the intro page', BASE_HALF_W - 700, BASE_HALF_H - 100 + infoPageYOffset + UI_Y_SHIFT);
 
         fill('#ffffff');
         rect(introBackButton.x, introBackButton.y, introBackButton.w, introBackButton.h, 16);
@@ -373,6 +393,7 @@ q5.draw = function() {
         textSize(34);
         text('Back to Intro Page', introBackButton.x + introBackButton.w / 2, introBackButton.y + introBackButton.h / 2);
 
+        pop();
         camera.on();
         return;
     }
@@ -381,6 +402,8 @@ q5.draw = function() {
         let colors = ['red', 'pastel yellow', 'green', 'blue', 'pink'];
     let i = floor(frameCount / 20) % colors.length;
     background(colors[i]);
+    push();
+    scale(uiScale);
     ladybug.changeAni('won');
 
         let discoFrame = floor(frameCount / 12) % 2;
@@ -395,11 +418,12 @@ q5.draw = function() {
     strokeWeight(4);
     textSize(80);
     textAlign(CENTER, CENTER);
-    text("YOU WIN! 🎉", width / 2-650, height / 2-400, 800, 600);
+    text("YOU WIN! 🎉", BASE_HALF_W - 650, BASE_HALF_H - 400 + UI_Y_SHIFT, 800, 600);
 
     fill('white');
     textSize(32);
     text("Press ENTER to reset", winResetTextX, winResetTextY);
+    pop();
 }
 
     fill('gold');
